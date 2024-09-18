a1428eae9736e89d8af48656199c0202a43dec1d
Max Rataj <maxmilianrataj@gmail.com>
2024-09-18 10:43:31 +0200

    Implement WriteToJson() logic

--- colors_io/ColorWriter.cs
+++ colors_io/ColorWriter.cs
@@ -1,10 +1,16 @@
-﻿using System;
+﻿using System.Text.Json;
+
 namespace colors_io
 {
 	public class ColorWriter
 	{
-		public ColorWriter()
+		public static void WriteToJsonFile(string filePath, List<ColorValue> colors)
 		{
+			var options = new JsonSerializerOptions { WriteIndented = true };
+
+			using StreamWriter streamWriter = new StreamWriter(filePath);
+			using Utf8JsonWriter jsonWriter = new Utf8JsonWriter(streamWriter.BaseStream, new JsonWriterOptions { Indented = true });
+			JsonSerializer.Serialize(jsonWriter, colors, options);
 		}
 	}
 }
